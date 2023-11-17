20e0cc81bdfc73fa3a6762c2599b6bed7744dcde
sebek001 <gidaszewski.sebastian@gmail.com>
2023-11-17 23:19:26 +0100

    three steps and about us

--- src/components/Home/HomeAboutUs.jsx
+++ src/components/Home/HomeAboutUs.jsx
@@ -1,9 +1,21 @@
-
-
 export default function HomeAboutUs() {
   return (
-    <div>
-      
-    </div>
-  )
+    <section className="aboutUs" id="o_nas">
+      <div className="aboutUs__left">
+        <h2 className="aboutUs__left-title">O nas</h2>
+        <img
+          className="header__main-decoration"
+          alt="decoration image"
+          src="../../../src/assets/Decoration.svg"
+        />
+        <p className="aboutUs__left-text">Nori grape silver beet broccoli kombu beet greens fava bean potato quandong celery. Bunya nuts black-eyed pea prairie turnip leek lentil turnip greens parsnip.</p>
+        <img
+          className="aboutUs__left-signature"
+          alt="signature"
+          src="../../../src/assets/Signature.svg"
+        />
+      </div>
+      <div className="aboutUs__right" />
+    </section>
+  );
 }
--- src/components/Home/HomeSimpleSteps.jsx
+++ src/components/Home/HomeSimpleSteps.jsx
@@ -1,9 +1,44 @@
-
+import { Link } from "react-router-dom";
 
 export default function HomeSimpleSteps() {
+  const steps = [
+    { title: "Wybierz rzeczy", desc: <>ubrania, zabawki,<br/> sprzęt i inne</> },
+    { title: "Spakuj je", desc: <>skorzystaj z <br/>worków na śmieci</> },
+    { title: <>Zdecyduj komu<br />
+    chcesz pomóc</>, desc: <>wybierz zaufane <br/>miejsce</> },
+    {
+      title: "Zamów kuriera",
+      desc: <>
+      kurier przyjedzie
+      <br />
+      w dogodnym terminie
+    </>,
+    },
+  ];
   return (
-    <div>
-      
-    </div>
-  )
+    <section className="simpleSteps" id="o_co_chodzi">
+      <h2 className="simpleSteps__title">Wystarczą 4 proste kroki</h2>
+      <img
+        className="header__main-decoration"
+        alt="decoration image"
+        src="../../../src/assets/Decoration.svg"
+      />
+      <div className="simpleSteps__elements">
+        {steps.map((step, index) => (
+          <div className="simpleSteps__elements-step" key={index}>
+            <img
+              className="simpleSteps__elements-step-img"
+              alt="step-img"
+              src={`../../../src/assets/Icon-${index + 1}.svg`}
+            />
+            <h3 className="simpleSteps__elements-step-title">{step.title}</h3>
+            <p className="simpleSteps__elements-step-desc">{step.desc}</p>
+          </div>
+        ))}
+      </div>
+      <Link to="/logowanie" className="header__main-buttons-el">
+            ODDAJ <br /> RZECZY
+          </Link>
+    </section>
+  );
 }
