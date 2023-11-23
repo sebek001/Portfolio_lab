7424505bcf79a2775f43ac77cb4b5a286d8ab2ae
sebek001 <gidaszewski.sebastian@gmail.com>
2023-11-23 22:56:24 +0100

    layout strony all done

--- src/components/Home/HomeWhoWeHelp.jsx
+++ src/components/Home/HomeWhoWeHelp.jsx
@@ -3,54 +3,60 @@ import supabase from "../../services/supabase";
 
 export default function HomeWhoWeHelp() {
   const [selectedItem, setSelectedItem] = useState("Fundacjom");
+  const [selectedItemIndex, setSelectedItemIndex] = useState(0);
   const [selectedKey, setSelectedKey] = useState("fundations");
   const items = [
     { fundations: "Fundacjom" },
     { organizations: "Organizacjom pozarządowym" },
     { locals: "Lokalnym zbiórkom" },
   ];
-  const [data, setData] = useState({
+  const [data, setData] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const itemsPerPage = 3;
+  const texts = {
     fundations: {
       desc: "W naszej bazie znajdziesz listę zweryfikowanych Fundacji, z którymi współpracujemy. Możesz sprawdzić czym się zajmują, komu pomagają i czego potrzebują.",
-      items: [],
     },
     organizations: {
       desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation.",
-      items: [],
     },
     locals: {
       desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation.",
-      items: [],
     },
-  });
+  };
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
-    items.map((item) => getData(item));
-  }, []);
+    getData(selectedKey);
+  }, [selectedKey]);
 
   async function getData(type) {
+    setLoading(true);
     let { data, error } = await supabase
       .from("data")
       .select("*")
       .eq("type", type);
 
+    setLoading(false);
+
     if (!error) {
-      setData((prev) => ({
-        ...prev,
-        [type]: { ...prev[type], items: data },
-      }));
+      setData(data);
     } else {
       console.error("Something went wrong", error.message);
     }
   }
 
-  function selectItem(value, key) {
+
+  function selectItem(value, key, index) {
     setSelectedItem(value);
     setSelectedKey(key);
+    setCurrentPage(1);
+    setSelectedItemIndex(index);
   }
 
+
   return (
-    <section className="whoWeHelp">
+    <section className="whoWeHelp" id='fundacje'>
       <h2 className="whoWeHelp__title">Komu pomagamy?</h2>
       <img
         className="header__main-decoration"
@@ -58,24 +64,71 @@ export default function HomeWhoWeHelp() {
         src="../../../src/assets/Decoration.svg"
       />
       <div className="whoWeHelp__selection">
-        {items.map((item) => (
-          <p
-            key={Object.values(item)[0]}
-            onClick={() =>
-              selectItem(Object.values(item)[0], Object.keys(item)[0])
-            }
-            className="whoWeHelp__selection-item"
-          >
-            {Object.values(item)[0]}
-          </p>
-        ))}
-        <div className='whoWeHelp__data'>
-          {selectedItem && selectedKey && (
-            <div className="whoWeHelp__data-description">{data[selectedKey].desc}</div>
-          )}
-          {/* mapa organizacji */}
-        </div>
+  {items.map((item, index) => (
+    <p
+      key={Object.values(item)[0]}
+      onClick={() =>
+        selectItem(Object.values(item)[0], Object.keys(item)[0], index)
+      }
+      className={`whoWeHelp__selection-item ${
+        selectedItemIndex === index ? "selected" : ""
+      }`}
+    >
+      {Object.values(item)[0]}
+    </p>
+  ))}
+</div>
+      <div className="whoWeHelp__data">
+        {loading && <p>Ładowanie danych...</p>}
+        {selectedItem && selectedKey && !loading && (
+          <div className="whoWeHelp__data-description">
+            {texts[selectedKey].desc}
+          </div>
+        )}
+        {selectedItem &&
+          selectedKey &&
+          !loading &&
+          data
+            .sort((a, b) => a.id - b.id)
+            .slice((currentPage - 1) * itemsPerPage, currentPage * itemsPerPage)
+            .map((org, index) => (
+              <div key={org.id} className="whoWeHelp__data-organizations">
+                <div className="whoWeHelp__data-organizations-left">
+                  <div className="whoWeHelp__data-organizations-name">
+                    {org.name}
+                  </div>
+                  <div className="whoWeHelp__data-organizations-goals">
+                    {org.goals}
+                  </div>
+                </div>
+                <div className="whoWeHelp__data-organizations-needs">
+                  {org.needs}
+                </div>
+              </div>
+            ))}
+        {!loading && selectedItem && selectedKey && data.length === 0 && (
+          <p>Brak dostępnych danych.</p>
+        )}
       </div>
+      {!loading &&
+        selectedItem &&
+        selectedKey &&
+        data.length > itemsPerPage && (
+          <div className="pagination">
+            {Array.from({
+              length: Math.ceil(data.length / itemsPerPage),
+            }).map((_, index) => (
+              <button
+                className="pagination__button"
+                key={index + 1}
+                onClick={() => setCurrentPage(index + 1)}
+                disabled={currentPage === index + 1}
+              >
+                {index + 1}
+              </button>
+            ))}
+          </div>
+        )}
     </section>
   );
 }
